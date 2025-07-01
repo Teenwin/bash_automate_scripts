63ff87c762ffef8ebb2c6d699dae83cfccc1abc5
Dmitry <dimka.ru.2010@gmail.com>
2025-07-01 12:33:06 +0300

    remove  word show and add information to readmifile

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 https://roadmap.sh/projects/server-stats
 to run script make him executable and start with https://wikipedia.org/wiki/Bourne_shell
+
+or `bash ./server_stats.sh`
--- server_stats.sh
+++ server_stats.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-echo "-------------show load average----------------";
+echo "-------------load average----------------";
 uptime | awk '{print $8,$9,$10}';
-echo "-------------show free and use memory----------------";
+echo "-------------free and use memory----------------";
 free -h | awk '{print $1,$2,$3}' | sed 's/total/type/';
-echo "-------------show free space----------------";
-df -h | awk '{print $1,$2,$3,$5}' | grep -v tmpfs;
-echo "-------------show five process by memory use----------------";
+echo "-------------free space----------------";
+df -h | awk '{print $1,$2,$3,$5}' | grep -v tmpfs | grep dev;
+echo "-------------five process by memory use----------------";
 ps -Ao %mem,user,pid,%cpu,time,comm --sort=-%mem | head -n 6;
-echo "-------------show five process by proc----------------";
+echo "-------------five process by proc----------------";
 ps -Ao pcpu,user,pid,%mem,time,comm --sort=-pcpu | head -n 6;
-echo "-------------show description version----------------";
+echo "-------------description version----------------";
 lsb_release -a | grep Description;
-echo "-------------show login users----------------";
+echo "-------------login users----------------";
 w -hip;
